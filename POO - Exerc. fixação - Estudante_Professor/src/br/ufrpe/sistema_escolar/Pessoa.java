78d37ca333ac250571e93c5ef95c28706490a198
Leandro M. Nascimento <leandro.marques@gmail.com>
2017-06-16 14:38:39 -0300

    Commit da classe Pessoa

--- POO - Exerc. fixação - Estudante_Professor/src/br/ufrpe/sistema_escolar/Pessoa.java
+++ POO - Exerc. fixação - Estudante_Professor/src/br/ufrpe/sistema_escolar/Pessoa.java
@@ -1,5 +1,31 @@
 package br.ufrpe.sistema_escolar;
 
 public class Pessoa {
+    
+    private String nome;
+    private String endereco;
+    
+    public Pessoa(String nome, String endereco) {
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+    
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+    /**
+     * @return the endereco
+     */
+    public String getEndereco() {
+        return endereco;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("Nome: %s. Endereço: %s", this.nome, this.endereco);
+    }
 
 }
