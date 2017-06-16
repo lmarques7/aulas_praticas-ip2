35d3d727bae836010357f57b3927daf17df44218
Leandro M. Nascimento <leandro.marques@gmail.com>
2017-06-16 15:03:05 -0300

    Classe estudante pronta

---
+++ POO - Exerc. fixação - Estudante_Professor/src/br/ufrpe/sistema_escolar/Estudante.java
@@ -0,0 +1,58 @@
+package br.ufrpe.sistema_escolar;
+
+public class Estudante extends Pessoa {
+
+    private int numCursos;
+    private String[] cursos;
+    private double[] notas;
+    
+    public Estudante(String nome, String endereco) {
+        super(nome, endereco);
+        
+        this.numCursos = 0;
+        this.cursos = new String[20];
+        this.notas = new double[20];
+    }
+    
+    public boolean addCursoNota(String curso, double nota) {
+        boolean adicionar = true;
+        for (int i = 0; i < this.numCursos && adicionar; i++) {
+            if (this.cursos[i].equals(curso)) {
+                adicionar = false;
+            }
+        }
+        
+        if (adicionar) {
+            this.cursos[this.numCursos] = curso;
+            this.notas[this.numCursos] = nota;
+            this.numCursos++;
+        }
+        return adicionar;
+    }
+    
+    public double getNotaMedia() {
+        double resultado = 0;
+        for (int i = 0; i < this.numCursos; i++) {
+            resultado += this.notas[i];
+        }
+        if (this.numCursos > 0) {
+            resultado = resultado / this.numCursos;
+        }
+        return resultado;
+    }
+    
+    @Override
+    public String toString() {
+        String resultado = super.toString() + "\n";
+        
+        for (int i = 0; i < this.numCursos; i++) {
+            resultado += "\tCurso: " + this.cursos[i] + ". Nota: " + this.notas[i] + "\n";
+        }
+        
+        return resultado;
+    }
+    
+    public void imprimeNotas() {
+        System.out.println(this.toString());
+    }
+}
---
+++ POO - Exerc. fixação - Estudante_Professor/src/br/ufrpe/sistema_escolar/Teste.java
@@ -0,0 +1,9 @@
+package br.ufrpe.sistema_escolar;
+
+public class Teste {
+
+    public static void main(String[] args) {
+
+    }
+
+}
