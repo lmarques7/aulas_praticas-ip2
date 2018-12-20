ab0f4a8df3290dbfdd0665c706b213c21b471c11
Leandro M. Nascimento <leandro.marques@gmail.com>
2018-12-20 12:20:27 -0300

    Exemplo produtor-consumidor

---
+++ POO - Aula - Java concurrency/src/producer_consumer/ProducerConsumerExample.java
@@ -0,0 +1,9 @@
+package producer_consumer;
+
+public class ProducerConsumerExample {
+    public static void main(String[] args) {
+        Drop drop = new Drop();
+        (new Thread(new Producer(drop))).start();
+        (new Thread(new Consumer(drop))).start();
+    }
+}
